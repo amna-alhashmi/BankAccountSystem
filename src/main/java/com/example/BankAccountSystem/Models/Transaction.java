9c4ba3ae4074ad4db23c446b5052e45cb9ba5364
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-21 16:10:45 +0400

    remove setter and getter

--- src/main/java/com/example/BankAccountSystem/Models/Transaction.java
+++ src/main/java/com/example/BankAccountSystem/Models/Transaction.java
@@ -20,27 +20,5 @@ public class Transaction extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "Account_id",referencedColumnName = "id")
     Account account;
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Double getFees() {
-        return fees;
-    }
-
-    public void setFees(Double fees) {
-        this.fees = fees;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
+   
 }

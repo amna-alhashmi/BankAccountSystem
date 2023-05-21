6cf857c2f3ecb433e2eaa79aa202e2c07c2c982f
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-21 16:25:28 +0400

    generate setter and getter for transaction request

--- src/main/java/com/example/BankAccountSystem/RequestObject/TransactionRequest.java
+++ src/main/java/com/example/BankAccountSystem/RequestObject/TransactionRequest.java
@@ -4,12 +4,43 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
-@Getter
+
 @Data
 public class TransactionRequest {
     Double amount;
     Long accountNumber;
     String isActive;
     Double fees;
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(String isActive) {
+        this.isActive = isActive;
+    }
+
+    public Double getFees() {
+        return fees;
+    }
+
+    public void setFees(Double fees) {
+        this.fees = fees;
+    }
 }

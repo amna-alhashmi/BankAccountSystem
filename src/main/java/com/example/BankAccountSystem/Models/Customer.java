b4968331aa3f015030f0b68e09e146317dfa444f
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-21 16:04:40 +0400

    remove generate setter and getter

--- src/main/java/com/example/BankAccountSystem/Models/Customer.java
+++ src/main/java/com/example/BankAccountSystem/Models/Customer.java
@@ -20,44 +20,5 @@ public class Customer extends BaseEntity{
     String gender;
     Integer phoneNumber;
 
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public Integer getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Integer phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+    
 }

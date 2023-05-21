614dc64f249a3ebb32afb181699a6c77a8203025
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-21 16:02:31 +0400

    remove generate setter and getter in model creditcard

--- src/main/java/com/example/BankAccountSystem/Models/CreditCard.java
+++ src/main/java/com/example/BankAccountSystem/Models/CreditCard.java
@@ -19,27 +19,5 @@ public class CreditCard extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "Customer_id",referencedColumnName = "id")
     Customer customer;
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public Integer getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(Integer accountNumber) {
-        this.accountNumber = accountNumber;
-    }
+  
 }

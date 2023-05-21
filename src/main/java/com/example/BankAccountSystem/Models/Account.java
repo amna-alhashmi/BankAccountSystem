e1a8e020a9d4c36dc1bfbcb6157d668d34cbdefe
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-21 15:59:59 +0400

    remove generate setter and getter

--- src/main/java/com/example/BankAccountSystem/Models/Account.java
+++ src/main/java/com/example/BankAccountSystem/Models/Account.java
@@ -20,27 +20,5 @@ public class Account extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "Customer_id",referencedColumnName = "id")
     Customer customer;
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Long getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(Long accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public Double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(Double balance) {
-        this.balance = balance;
-    }
+    
 }

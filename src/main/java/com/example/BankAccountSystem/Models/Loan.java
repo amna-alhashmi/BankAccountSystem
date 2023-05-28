c4e40c1617f5fc8e3e34630099d5fb66da80122b
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-28 14:35:41 +0400

    add notnull in Loan model

--- src/main/java/com/example/BankAccountSystem/Models/Loan.java
+++ src/main/java/com/example/BankAccountSystem/Models/Loan.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
@@ -13,8 +14,12 @@ import javax.persistence.*;
 public class Loan extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @NotNull
     Integer id;
+    @NotNull
     Double amount;
+    @NotNull
     Double interest;
     @ManyToOne
     @JoinColumn(name = "Customer_id",referencedColumnName = "id")

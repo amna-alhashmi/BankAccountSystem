c4fcb830fcbf287879aed1be8ca8b9102015819b
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-28 14:19:31 +0400

    add requestBody for creditCard Controller

--- src/main/java/com/example/BankAccountSystem/Controller/CreditCardController.java
+++ src/main/java/com/example/BankAccountSystem/Controller/CreditCardController.java
@@ -1,22 +1,20 @@
 package com.example.BankAccountSystem.Controller;
 
+import com.example.BankAccountSystem.Models.CreditCard;
 import com.example.BankAccountSystem.Service.CreditCardService;
 import com.example.BankAccountSystem.Service.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
-    @RestController
+@RestController
     @RequestMapping(value = "CreditCard")
     public class CreditCardController {
         @Autowired
         CreditCardService creditCardService;
         @RequestMapping(value = "CreditCardApplicationForCustomer", method = RequestMethod.POST)
-        public String addCreditCardForCustomer() {
-            creditCardService.addCreditCardForCustomer();
+        public String addCreditCardForCustomer(@RequestBody CreditCard creditCard) {
+            creditCardService.addCreditCardForCustomer(creditCard);
 
             return "CreditCard add successful";
         }

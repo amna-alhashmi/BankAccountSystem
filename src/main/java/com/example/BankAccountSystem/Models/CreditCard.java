6838a73e88ca7dec7f29196c7f4889a2d24ca14b
amna-alhashmi <amnaalhashmi98@gmail.com>
2023-05-28 14:22:21 +0400

    add nonull for CreeditCard model

--- src/main/java/com/example/BankAccountSystem/Models/CreditCard.java
+++ src/main/java/com/example/BankAccountSystem/Models/CreditCard.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -13,6 +14,7 @@ import javax.persistence.*;
 public class CreditCard extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
     Integer id;
     String customerName;
     Integer accountNumber;
